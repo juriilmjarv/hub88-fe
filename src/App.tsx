17e905d925d0db653ef0008df9841c79a0c680c9
Juri Ilmjarv <juura.ilmjarv@gmail.com>
2022-11-29 16:01:07 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from "@apollo/client";
+
+const FILMS_QUERY = gql`
+{
+	countries {
+		name,
+		code
+	}
+}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const { data, loading, error } = useQuery(FILMS_QUERY);
+	console.log(data);
+	console.log(loading)
+
+	if (loading) return <>"Loading..."</>;
+	if (error) return <pre>{error.message}</pre>
+
+	return (
+		<div>
+			<h1>Hub88</h1>
+			<ul>
+				{data.countries.map((launch: any) => (
+					<li key={launch.code}>{launch.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
